3e9ca4b7c6161c45bcc101d9f5b07307da886722
felixtsu <humaeks@gmail.com>
2019-05-23 17:36:41 +0800

    remove demo algorithm

--- main.ts
+++ main.ts
@@ -1,23 +1,2 @@
-let decision = 0
-let candidates: number[] = []
 custom.onPlayGame(function () {
-    candidates = []
-    if (!(custom.frontIsBlocked())) {
-        candidates.push(2)
-        candidates.push(3)
-        candidates.push(4)
-        candidates.push(5)
-    }
-    if (!(custom.leftIsBlocked())) {
-        candidates.push(0)
-    }
-    if (!(custom.rightIsBlocked())) {
-        candidates.push(1)
-    }
-    decision = candidates[Math.randomRange(0, candidates.length - 1)]
-    if (decision == 0) {
-        custom.turnLeft()
-    } else if (decision == 1) {
-        custom.turnRight()
-    }
 })
