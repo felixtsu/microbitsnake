61218a2759e7ef9f88e23f3306fc651553c36300
felixtsu <humaeks@gmail.com>
2019-05-24 20:25:19 +0800

    archive old version

---
+++ snake.old.ts
@@ -0,0 +1,297 @@
+/*
+ * Snake by Cubicbird helper blocks
+ */
+//% weight=100 color=#F3A21B icon="\uF4BA"
+namespace snake {
+
+    let successfulGen = false
+    let food: number[] = []
+    let score = 0
+    let isFacingBlock = false
+    let isLeftSideBlocked = false
+    let isRightSideBlocked = false
+    let direction = "U"
+    let snakeX = [2, 2, 2]
+    let snakeY = [2, 3, 4]
+    let head = [2, 2]
+    let gameEnd = false
+    let ateFood = false
+    let cycle = 0
+
+    function initSnakeGame() {
+        drawSnake()
+        genFood()
+        drawFood(0)
+    }
+
+    /**
+     * make a left turn
+     */
+    //% block
+    export function turnLeft() {
+        if (direction == "U") {
+            direction = "L"
+        } else if (direction == "D") {
+            direction = "R"
+        } else if (direction == "L") {
+            direction = "D"
+        } else {
+            direction = "U"
+        }
+    }
+
+
+    /**
+     * make a right turn
+     */
+    //% block
+    export function turnRight() {
+        if (direction == "U") {
+            direction = "R"
+        } else if (direction == "D") {
+            direction = "L"
+        } else if (direction == "L") {
+            direction = "U"
+        } else {
+            direction = "D"
+        }
+    }
+
+    /**
+     * return if the front is blocked
+     */
+    //% block
+    export function frontIsBlocked(): boolean {
+        return isFacingBlock;
+    }
+
+    /**
+     * return if the left side is blocked
+     */
+    //% block
+    export function leftIsBlocked(): boolean {
+        return isLeftSideBlocked;
+    }
+    /**
+     * return if the right side is blocked
+     */
+    //% block
+    export function rightIsBlocked(): boolean {
+        return isRightSideBlocked;
+    }
+
+    function updateFacingBlockV2() {
+        if (direction == "U") {
+            if (head[1] == 0) {
+                isFacingBlock = true
+            } else if (led.point(head[0], head[1] - 1) && !(food[0] == head[0] && food[1] == head[1] - 1)) {
+                isFacingBlock = true
+            }
+        }
+        if (direction == "D") {
+            if (head[1] == 4) {
+                isFacingBlock = true
+            } else if (led.point(head[0], head[1] + 1) && !(food[0] == head[0] && food[1] == head[1] + 1)) {
+                isFacingBlock = true
+            }
+        }
+        if (direction == "L") {
+            if (head[0] == 0) {
+                isFacingBlock = true
+            } else if (led.point(head[0] - 1, head[1]) && !(food[0] == head[0] - 1 && food[1] == head[1])) {
+                isFacingBlock = true
+            }
+        }
+        if (direction == "R") {
+            if (head[0] == 4) {
+                isFacingBlock = true
+            } else if (led.point(head[0] + 1, head[1]) && !(food[0] == head[0] + 1 && food[1] == head[1])) {
+                isFacingBlock = true
+            }
+        }
+
+    }
+    function updateLeftFacingBlockV2() {
+        if (direction == "U") {
+            if (head[0] == 0) {
+                isLeftSideBlocked = true
+            } else if (led.point(head[0] - 1, head[1]) && !(food[0] == head[0] - 1 && food[1] == head[1])) {
+                isLeftSideBlocked = true
+            }
+        }
+        if (direction == "D") {
+            if (head[0] == 4) {
+                isLeftSideBlocked = true
+            } else if (led.point(head[0] + 1, head[1]) && !(food[0] == head[0] + 1 && food[1] == head[1])) {
+                isLeftSideBlocked = true
+            }
+        }
+        if (direction == "L") {
+            if (head[1] == 4) {
+                isLeftSideBlocked = true
+            } else if (led.point(head[0], head[1] + 1) && !(food[0] == head[0] && food[1] == head[1] + 1)) {
+                isLeftSideBlocked = true
+            }
+        }
+        if (direction == "R") {
+            if (head[1] == 0) {
+                isLeftSideBlocked = true
+            } else if (led.point(head[0], head[1] - 1) && !(food[0] == head[0] && food[1] == head[1] - 1)) {
+                isLeftSideBlocked = true
+            }
+        }
+    }
+
+    function updateRightFacingBlockV2() {
+        if (direction == "U") {
+            if (head[0] == 4) {
+                isRightSideBlocked = true
+            } else if (led.point(head[0] + 1, head[1]) && !(food[0] == head[0] + 1 && food[1] == head[1])) {
+                isRightSideBlocked = true
+            }
+        }
+        if (direction == "D") {
+            if (head[0] == 0) {
+                isRightSideBlocked = true
+            } else if (led.point(head[0] - 1, head[1]) && !(food[0] == head[0] - 1 && food[1] == head[1])) {
+                isRightSideBlocked = true
+            }
+        }
+        if (direction == "L") {
+            if (head[1] == 0) {
+                isRightSideBlocked = true
+            } else if (led.point(head[0], head[1] - 1) && !(food[0] == head[0] && food[1] == head[1] - 1)) {
+                isRightSideBlocked = true
+            }
+        }
+        if (direction == "R") {
+            if (head[1] == 4) {
+                isRightSideBlocked = true
+            } else if (led.point(head[0], head[1] + 1) && !(food[0] == head[0] && food[1] == head[1] + 1)) {
+                isRightSideBlocked = true
+            }
+        }
+    }
+
+    function moveHead() {
+        if (direction == "U") {
+            head[1] = head[1] - 1
+        } else if (direction == "D") {
+            head[1] = head[1] + 1
+        } else if (direction == "L") {
+            head[0] = head[0] - 1
+        } else {
+            head[0] = head[0] + 1
+        }
+        snakeX.insertAt(0, head[0])
+        snakeY.insertAt(0, head[1])
+    }
+    function drawSnake() {
+        for (let j = 0; j <= snakeX.length - 1; j++) {
+            led.plot(snakeX[j], snakeY[j])
+        }
+    }
+    function isDead() {
+        if (head[0] < 0 || head[0] > 4) {
+            endGame()
+        }
+        if (head[1] < 0 || head[1] > 4) {
+            endGame()
+        }
+        if (led.point(head[0], head[1])) {
+            if (food[0] == head[0] && food[1] == head[1]) {
+                score += 1
+                ateFood = true
+            } else {
+                endGame()
+            }
+        }
+    }
+    function genFood() {
+        successfulGen = false
+        while (!(successfulGen)) {
+            let temp = [Math.randomRange(0, 4), Math.randomRange(0, 4)]
+            if (!(led.point(temp[0], temp[1]))) {
+                successfulGen = true
+                food[0] = temp[0]
+                food[1] = temp[1]
+            }
+        }
+    }
+    function drawFood(cycle: number) {
+        if (cycle % 2 == 0) {
+            led.plot(food[0], food[1])
+        } else {
+            led.unplot(food[0], food[1])
+        }
+    }
+
+    function judgeGameEnd() {
+        if (gameEnd) {
+            basic.pause(10000000)
+        }
+    }
+
+    function endGame() {
+        gameEnd = true
+    }
+    function moveSnake() {
+        led.plot(head[0], head[1])
+        if (!(ateFood)) {
+            led.unplot(snakeX.pop(), snakeY.pop())
+        } else {
+            ateFood = false
+        }
+    }
+    function genFoodIfNecessary() {
+        if (ateFood) {
+            genFood()
+        }
+    }
+
+    let playGameHandler: () => void;
+
+    /**
+     * This is an event handler block
+     */
+    //% block="Make a move"
+    export function onPlayGame(handler: () => void) {
+        playGameHandler = handler
+    }
+
+    function updateStatus() {
+        cycle++
+        moveHead()
+        isDead()
+        judgeGameEnd()
+        genFoodIfNecessary()
+        moveSnake()
+
+        isFacingBlock = false
+        isLeftSideBlocked = false
+        isRightSideBlocked = false
+        updateFacingBlockV2()
+        updateLeftFacingBlockV2()
+        updateRightFacingBlockV2()
+    }
+
+    function render() {
+
+    }
+
+    //initSnakeGame()
+    /*
+        basic.forever(function () {
+            basic.pause(20)
+            if (cycle++ % 4 == 0) {
+                playGameHandler()    
+                updateStatus()
+                render()
+            } else {
+                drawFood(cycle)
+            }
+    
+        })
+        */
+}
+
--- snake.ts
+++ snake.ts
@@ -4,65 +4,271 @@
 //% weight=100 color=#F3A21B icon="\uF4BA"
 namespace snake {
 
-    let successfulGen = false
-    let food: number[] = []
-    let score = 0
-    let isFacingBlock = false
-    let isLeftSideBlocked = false
-    let isRightSideBlocked = false
-    let direction = "U"
-    let snakeX = [2, 2, 2]
-    let snakeY = [2, 3, 4]
-    let head = [2, 2]
-    let gameEnd = false
-    let ateFood = false
-    let cycle = 0
 
-    function initSnakeGame() {
-        drawSnake()
-        genFood()
-        drawFood(0)
+    export enum Mode {
+        MANUAL, PROGRAMMING
     }
 
+    class CoreEngine {
+        food: number[]
+        score: number
+        /**
+         * snake direction 
+         * 0 up
+         * 1 left
+         * 2 down
+         * 3 right
+         */
+        direction: number
+        snakeX: number[]
+        snakeY: number[]
+        tail: number[]
+        head: number[]
+        cycle: number
+        _gameEnd: boolean
+        makeAMoveHandler: () => void
+        public constructor() {
+            this.food = [3, 1]
+            this.score = 0
+            /**
+             * snake direction 
+             * 0 up
+             * 1 left
+             * 2 down
+             * 3 right
+             */
+            this.direction = 0
+            this.snakeX = [2, 2, 2]
+            this.snakeY = [2, 3, 4]
+            this.tail = [2, 4]
+            this.head = [2, 2]
+            this.cycle = 0
+            this._gameEnd = false
+        }
+
+        frontIsBlocked(): boolean {
+            return this.validUnoccypiedPosition(this.nextHead())
+        }
+
+        leftIsBlocked(): boolean {
+            this.turnLeft()
+            let result = this.validUnoccypiedPosition(this.nextHead())
+            this.turnRight()
+            return result
+        }
+
+        rightIsBlocked(): boolean {
+            this.turnRight()
+            let result = this.validUnoccypiedPosition(this.nextHead())
+            this.turnLeft()
+            return result
+        }
+
+
+        setMakeAMoveHandler(handler: () => void) {
+            this.makeAMoveHandler = handler
+        }
+
+        plotSnake() {
+            for (let i = 0; i < this.snakeX.length; i++) {
+                led.plot(this.snakeX[i], this.snakeY[i])
+            }
+        }
+
+        render() {
+            this.drawFood()
+            this.drawSnakeDelta()
+        }
+
+        turnLeft() {
+            this.direction = (this.direction + 1) % 4
+        }
+
+        turnRight() {
+            this.direction = (this.direction + 3) % 4
+        }
+
+        drawFood() {
+            if (this.cycle % 2 == 0) {
+                led.plot(this.food[0], this.food[1])
+            } else {
+                led.unplot(this.food[0], this.food[1])
+            }
+        }
+
+        drawSnakeDelta() {
+            led.plot(this.head[0], this.head[1])
+            led.unplot(this.tail[0], this.tail[1])
+        }
+
+        /**
+         * update game status
+         */
+        updateGame() {
+            this.cycle++
+            if (this.cycle % 4 == 0) {
+                if (this.makeAMoveHandler) {
+                    this.makeAMoveHandler()
+                }
+                this.moveHead()
+
+                if (this.validUnoccypiedPosition(this.head)) {
+                    this._gameEnd = true;
+                } else {
+                    if (!this.ateFood()) {
+                        this.moveTail()
+                    } else {
+                        this.generateFood()
+                    }
+                }
+            }
+        }
+
+        overlapWithSnake(coordinate: number[], headInclude: boolean): boolean {
+            let i = headInclude ? 0 : 1
+            for (; i < this.snakeX.length; i++) {
+                if (coordinate[0] == this.snakeX[i] && coordinate[1] == this.snakeY[i]) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        generateFood() {
+            this.food = []
+            do {
+                this.food[0] = Math.randomRange(0, 4)
+                this.food[1] = Math.randomRange(0, 4)
+            } while (this.overlapWithSnake(this.food, true))
+        }
+
+        validUnoccypiedPosition(head: number[]): boolean {
+            if (head[0] < 0 || head[0] > 4) {
+                return true
+            }
+            if (head[1] < 0 || head[1] > 4) {
+                return true
+            }
+            return this.overlapWithSnake(head, false)
+        }
+
+        ateFood(): boolean {
+            return this.head[0] == this.food[0] && this.head[1] == this.food[1]
+        }
+
+        nextHead(): number[] {
+            let _nextHead = [this.head[0], this.head[1]]
+            switch (this.direction) {
+                case 0:
+                    _nextHead[1] = _nextHead[1] - 1
+                    break
+                case 1:
+                    _nextHead[0] = _nextHead[0] - 1
+                    break
+                case 2:
+                    _nextHead[1] = _nextHead[1] + 1
+                    break
+                case 3:
+                    _nextHead[0] = _nextHead[0] + 1
+            }
+            return _nextHead
+        }
+
+        moveHead() {
+            this.head = this.nextHead()
+            this.snakeX.insertAt(0, this.head[0])
+            this.snakeY.insertAt(0, this.head[1])
+        }
+
+        moveTail() {
+            this.tail = [this.snakeX.pop(), this.snakeY.pop()]
+        }
+
+        gameEnd(): boolean {
+            return this._gameEnd
+
+        }
+
+        invokeHandler() {
+            this.makeAMoveHandler()
+        }
+
+    }
+
+    let coreEngine: CoreEngine
+
     /**
-     * make a left turn
-     */
+       * init snake game 
+       * in Manual Mode, press A to turn left, B to turn right
+       * in Programming Mode, write your decision making login in side a "Make a move" block
+       */
+    //% block
+    export function initSnakeGame(mode: Mode) {
+        coreEngine = new CoreEngine()
+
+        if (mode == Mode.MANUAL) {
+            input.onButtonPressed(Button.A, function () {
+                coreEngine.turnLeft()
+            })
+            input.onButtonPressed(Button.B, function () {
+                coreEngine.turnRight()
+            })
+            basic.forever(function () {
+                coreEngine.updateGame()
+                if (coreEngine.gameEnd()) {
+                    basic.pause(1000000)
+                } else {
+                    coreEngine.render()
+                    basic.pause(125)
+                }
+            })
+        } else if (mode == Mode.PROGRAMMING) {
+            basic.forever(function () {
+                coreEngine.updateGame()
+                if (coreEngine.gameEnd()) {
+                    basic.pause(1000000)
+                } else {
+                    coreEngine.render()
+                    basic.pause(50)
+                }
+            })
+        }
+
+        coreEngine.drawFood()
+        coreEngine.plotSnake()
+
+    }
+
+
+
+    /**
+    * This is an event handler block
+    */
+    //% block="Make a move"
+    export function onGameTick(handler: () => void) {
+        coreEngine.setMakeAMoveHandler(handler)
+    }
+    /**
+       * make a left turn
+       */
     //% block
     export function turnLeft() {
-        if (direction == "U") {
-            direction = "L"
-        } else if (direction == "D") {
-            direction = "R"
-        } else if (direction == "L") {
-            direction = "D"
-        } else {
-            direction = "U"
-        }
+        coreEngine.turnLeft()
     }
-
-
     /**
-     * make a right turn
-     */
+   * make a right turn
+   */
     //% block
     export function turnRight() {
-        if (direction == "U") {
-            direction = "R"
-        } else if (direction == "D") {
-            direction = "L"
-        } else if (direction == "L") {
-            direction = "U"
-        } else {
-            direction = "D"
-        }
+        coreEngine.turnRight()
     }
 
     /**
-     * return if the front is blocked
-     */
+    * return if the front is blocked
+    */
     //% block
     export function frontIsBlocked(): boolean {
-        return isFacingBlock;
+        return coreEngine.frontIsBlocked();
     }
 
     /**
@@ -70,228 +276,16 @@ namespace snake {
      */
     //% block
     export function leftIsBlocked(): boolean {
-        return isLeftSideBlocked;
+        return coreEngine.leftIsBlocked();
     }
     /**
      * return if the right side is blocked
      */
     //% block
     export function rightIsBlocked(): boolean {
-        return isRightSideBlocked;
+        return coreEngine.rightIsBlocked();
     }
 
-    function updateFacingBlockV2() {
-        if (direction == "U") {
-            if (head[1] == 0) {
-                isFacingBlock = true
-            } else if (led.point(head[0], head[1] - 1) && !(food[0] == head[0] && food[1] == head[1] - 1)) {
-                isFacingBlock = true
-            }
-        }
-        if (direction == "D") {
-            if (head[1] == 4) {
-                isFacingBlock = true
-            } else if (led.point(head[0], head[1] + 1) && !(food[0] == head[0] && food[1] == head[1] + 1)) {
-                isFacingBlock = true
-            }
-        }
-        if (direction == "L") {
-            if (head[0] == 0) {
-                isFacingBlock = true
-            } else if (led.point(head[0] - 1, head[1]) && !(food[0] == head[0] - 1 && food[1] == head[1])) {
-                isFacingBlock = true
-            }
-        }
-        if (direction == "R") {
-            if (head[0] == 4) {
-                isFacingBlock = true
-            } else if (led.point(head[0] + 1, head[1]) && !(food[0] == head[0] + 1 && food[1] == head[1])) {
-                isFacingBlock = true
-            }
-        }
 
-    }
-    function updateLeftFacingBlockV2() {
-        if (direction == "U") {
-            if (head[0] == 0) {
-                isLeftSideBlocked = true
-            } else if (led.point(head[0] - 1, head[1]) && !(food[0] == head[0] - 1 && food[1] == head[1])) {
-                isLeftSideBlocked = true
-            }
-        }
-        if (direction == "D") {
-            if (head[0] == 4) {
-                isLeftSideBlocked = true
-            } else if (led.point(head[0] + 1, head[1]) && !(food[0] == head[0] + 1 && food[1] == head[1])) {
-                isLeftSideBlocked = true
-            }
-        }
-        if (direction == "L") {
-            if (head[1] == 4) {
-                isLeftSideBlocked = true
-            } else if (led.point(head[0], head[1] + 1) && !(food[0] == head[0] && food[1] == head[1] + 1)) {
-                isLeftSideBlocked = true
-            }
-        }
-        if (direction == "R") {
-            if (head[1] == 0) {
-                isLeftSideBlocked = true
-            } else if (led.point(head[0], head[1] - 1) && !(food[0] == head[0] && food[1] == head[1] - 1)) {
-                isLeftSideBlocked = true
-            }
-        }
-    }
 
-    function updateRightFacingBlockV2() {
-        if (direction == "U") {
-            if (head[0] == 4) {
-                isRightSideBlocked = true
-            } else if (led.point(head[0] + 1, head[1]) && !(food[0] == head[0] + 1 && food[1] == head[1])) {
-                isRightSideBlocked = true
-            }
-        }
-        if (direction == "D") {
-            if (head[0] == 0) {
-                isRightSideBlocked = true
-            } else if (led.point(head[0] - 1, head[1]) && !(food[0] == head[0] - 1 && food[1] == head[1])) {
-                isRightSideBlocked = true
-            }
-        }
-        if (direction == "L") {
-            if (head[1] == 0) {
-                isRightSideBlocked = true
-            } else if (led.point(head[0], head[1] - 1) && !(food[0] == head[0] && food[1] == head[1] - 1)) {
-                isRightSideBlocked = true
-            }
-        }
-        if (direction == "R") {
-            if (head[1] == 4) {
-                isRightSideBlocked = true
-            } else if (led.point(head[0], head[1] + 1) && !(food[0] == head[0] && food[1] == head[1] + 1)) {
-                isRightSideBlocked = true
-            }
-        }
-    }
-
-    function moveHead() {
-        if (direction == "U") {
-            head[1] = head[1] - 1
-        } else if (direction == "D") {
-            head[1] = head[1] + 1
-        } else if (direction == "L") {
-            head[0] = head[0] - 1
-        } else {
-            head[0] = head[0] + 1
-        }
-        snakeX.insertAt(0, head[0])
-        snakeY.insertAt(0, head[1])
-    }
-    function drawSnake() {
-        for (let j = 0; j <= snakeX.length - 1; j++) {
-            led.plot(snakeX[j], snakeY[j])
-        }
-    }
-    function isDead() {
-        if (head[0] < 0 || head[0] > 4) {
-            endGame()
-        }
-        if (head[1] < 0 || head[1] > 4) {
-            endGame()
-        }
-        if (led.point(head[0], head[1])) {
-            if (food[0] == head[0] && food[1] == head[1]) {
-                score += 1
-                ateFood = true
-            } else {
-                endGame()
-            }
-        }
-    }
-    function genFood() {
-        successfulGen = false
-        while (!(successfulGen)) {
-            let temp = [Math.randomRange(0, 4), Math.randomRange(0, 4)]
-            if (!(led.point(temp[0], temp[1]))) {
-                successfulGen = true
-                food[0] = temp[0]
-                food[1] = temp[1]
-            }
-        }
-    }
-    function drawFood(cycle: number) {
-        if (cycle % 2 == 0) {
-            led.plot(food[0], food[1])
-        } else {
-            led.unplot(food[0], food[1])
-        }
-    }
-
-    function judgeGameEnd() {
-        if (gameEnd) {
-            basic.pause(10000000)
-        }
-    }
-
-    function endGame() {
-        gameEnd = true
-    }
-    function moveSnake() {
-        led.plot(head[0], head[1])
-        if (!(ateFood)) {
-            led.unplot(snakeX.pop(), snakeY.pop())
-        } else {
-            ateFood = false
-        }
-    }
-    function genFoodIfNecessary() {
-        if (ateFood) {
-            genFood()
-        }
-    }
-
-    let playGameHandler: () => void;
-
-    /**
-     * This is an event handler block
-     */
-    //% block="Make a move"
-    export function onPlayGame(handler: () => void) {
-        playGameHandler = handler
-    }
-
-    function updateStatus() {
-        cycle++
-        moveHead()
-        isDead()
-        judgeGameEnd()
-        genFoodIfNecessary()
-        moveSnake()
-
-        isFacingBlock = false
-        isLeftSideBlocked = false
-        isRightSideBlocked = false
-        updateFacingBlockV2()
-        updateLeftFacingBlockV2()
-        updateRightFacingBlockV2()
-    }
-
-    function render() {
-
-    }
-
-    //initSnakeGame()
-    /*
-        basic.forever(function () {
-            basic.pause(20)
-            if (cycle++ % 4 == 0) {
-                playGameHandler()    
-                updateStatus()
-                render()
-            } else {
-                drawFood(cycle)
-            }
-    
-        })
-        */
-}
-
+} 
